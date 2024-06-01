53f905e3d597be68a66af906bd69f4f1324ba655
rndmzd <rndmzd@protonmail.com>
2024-05-31 21:16:23 -0500

    Handle stop-countdown event.

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,4 +1,3 @@
-// src/index.ts
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
@@ -8,23 +7,33 @@ import { startCountdown } from './countdownTimer';
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:4000',
-        methods: ['GET', 'POST'],
-        allowedHeaders: ['Content-Type'],
-    }
+  cors: {
+    origin: 'http://localhost:4000',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+  }
 });
 
 app.use(cors({
-    origin: 'http://localhost:4000',
+  origin: 'http://localhost:4000',
 }));
+
+let countdownControl: ReturnType<typeof startCountdown> | null = null;
 
 io.on('connection', (socket) => {
   console.log('a user connected');
 
   socket.on('start-countdown', (duration: number) => {
     console.log(`Starting countdown for ${duration} seconds`);
-    startCountdown(io, duration);
+    countdownControl = startCountdown(io, duration);
+  });
+
+  socket.on('stop-countdown', () => {
+    console.log('Stopping countdown');
+    if (countdownControl) {
+      countdownControl.stop();
+      countdownControl = null;
+    }
   });
 
   socket.on('disconnect', () => {
