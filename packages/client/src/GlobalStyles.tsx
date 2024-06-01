d15f30e69b0357a3f348179c369c2554a8d578a4
rndmzd <rndmzd@protonmail.com>
2024-05-31 21:52:25 -0500

    Add GlobalStyles.

---
+++ packages/client/src/GlobalStyles.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { GlobalStyles as MuiGlobalStyles } from "@mui/material";
+
+const GlobalStyles = () => (
+  <MuiGlobalStyles
+    styles={{
+      body: {
+        margin: 0,
+        padding: 0,
+        fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      },
+      "*": {
+        boxSizing: "border-box",
+      },
+    }}
+  />
+);
+
+export default GlobalStyles;
--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './styles.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import GlobalStyles from "./GlobalStyles";
+import "./styles.css";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container!); // `!` asserts that container is not null
-root.render(<App />);
+root.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>
+);
