08bcd2995439a93d4d88c1e7aeb3ba1b8f928bba
rndmzd <rndmzd@protonmail.com>
2024-06-05 02:48:40 -0500

    Formatting.

--- packages/server/src/generateSecret.js
+++ packages/server/src/generateSecret.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto');
+const crypto = require("crypto");
 
-const secret = crypto.randomBytes(64).toString('hex');
+const secret = crypto.randomBytes(64).toString("hex");
 console.log(secret);
