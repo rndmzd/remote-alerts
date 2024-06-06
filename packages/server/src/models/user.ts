e1e8d4eafbd1aabf657d8acdbd67d7811b022a0a
rndmzd <rndmzd@protonmail.com>
2024-06-06 15:33:39 -0500

    Add User model.

---
+++ packages/server/src/models/user.ts
@@ -0,0 +1,44 @@
+import { DataTypes, Model, Optional } from 'sequelize';
+import sequelize from '../db';
+
+interface UserAttributes {
+  id: number;
+  username: string;
+  password: string;
+}
+
+interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
+
+class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
+  public id!: number;
+  public username!: string;
+  public password!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'users',
+  }
+);
+
+export default User;
