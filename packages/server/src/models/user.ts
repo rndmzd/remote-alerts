c52779238f7818f3e0a82fbae11a3b60c481722f
rndmzd <rndmzd@protonmail.com>
2024-06-06 15:38:23 -0500

    SQLite doesn't use integer unsigned.

--- packages/server/src/models/user.ts
+++ packages/server/src/models/user.ts
@@ -21,7 +21,7 @@ class User extends Model<UserAttributes, UserCreationAttributes> implements User
 User.init(
   {
     id: {
-      type: DataTypes.INTEGER.UNSIGNED,
+      type: DataTypes.INTEGER,
       autoIncrement: true,
       primaryKey: true,
     },
