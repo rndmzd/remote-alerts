c6cd79cba7ff74bd2fb32f5d09888a9eae05d033
rndmzd <rndmzd@protonmail.com>
2024-06-06 15:38:16 -0500

    Remove duplicate server listen call.

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -66,10 +66,6 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
-
 sequelize.sync().then(() => {
   server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
